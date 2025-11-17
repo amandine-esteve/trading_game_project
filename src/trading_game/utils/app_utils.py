90fe65be75e38608d99c04974e95dbe05e2c5dfa
amandine-esteve <amandine.esteve.bernard@gmail.com>
2025-11-17 22:19:31 +0100

    improve stock simulation and stock attributes name/ticker on app

--- src/trading_game/utils/app_utils.py
+++ src/trading_game/utils/app_utils.py
@@ -1,14 +1,6 @@
-from trading_game.config.stock_pool import get_random_stock
 from trading_game.config.investor_pool import get_random_investors
 from trading_game.config.settings import NB_INVESTORS
-from trading_game.core.market import Stock
 from trading_game.core.street import Investor, Street, QuoteRequest #, LevelRequest, StateRequest
-
-
-def create_stock():
-    stock_data = get_random_stock()
-    stock = Stock(**stock_data)
-    return stock
 
 def create_street():
     investors_data = get_random_investors(NB_INVESTORS, unique_companies=True, mixed_types=True)
